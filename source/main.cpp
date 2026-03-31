73dfcaf9c99f208397b0d4a7b6b06e6df1dd2619
hellokugelblitz <hellokugelblitz@gmail.com>
2026-03-31 14:31:42 -0400

    working message popup

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+// SDL Includes
+#include <SDL3/SDL.h>
+
 int main(){
+	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Title", "Message", 0);
 	std::cout << "Build Test" << std::endl;
 }
